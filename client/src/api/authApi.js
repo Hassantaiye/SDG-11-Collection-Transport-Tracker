b704a27888a36b20653e0143ffc74e67b1c6729b
Hassan Taiye <hassantaiwo204@gmail.com>
2025-11-19 08:37:15 +0100

    update

--- client/src/api/authApi.js
+++ client/src/api/authApi.js
@@ -1,15 +1,28 @@
 import axios from "./axiosInstance";
 
 export const registerUser = async (userData) => {
-  const res = await axios.post("/api/auth/register", userData);
-  if (res.data.user?.token) localStorage.setItem("token", res.data.user.token);
-  return res.data.user;
+  try {
+    const res = await axios.post("/api/auth/register", userData);
+    if (res.data.user?.token) {
+      localStorage.setItem("token", res.data.user.token);
+    }
+    return res.data.user;
+  } catch (error) {
+    // Enhanced error throwing to preserve backend error messages
+    throw error;
+  }
 };
 
 export const loginUser = async (credentials) => {
-  const res = await axios.post("/api/auth/login", credentials);
-  if (res.data.user?.token) localStorage.setItem("token", res.data.user.token);
-  return res.data.user;
+  try {
+    const res = await axios.post("/api/auth/login", credentials);
+    if (res.data.user?.token) {
+      localStorage.setItem("token", res.data.user.token);
+    }
+    return res.data.user;
+  } catch (error) {
+    throw error;
+  }
 };
 
 export const getMeApi = async () => {
